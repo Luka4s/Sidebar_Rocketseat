453773a59cb6f71cecaa23cf6307e75f5f804fc7
Lucas Seidel <lucas.rodrigues28012003@gmail.com>
2023-03-19 12:20:07 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,36 @@
+const Open = document.getElementById('Open')
+const Side = document.getElementById('Side')
+const h2 = document.querySelectorAll('h2')
+
+const logo = document.getElementById('img')
+const gridtxt = document.getElementById('gridtxt')
+const pettxt = document.getElementById('pettxt')
+const usertxt = document.getElementById('usertxt')
+const vettxt = document.getElementById('vettxt')
+const settingstxt = document.getElementById('settingstxt')
+
+function Assignment(){
+
+    console.log('Click')
+    Side.style.width = '150px'
+    Side.style.backgroundColor ='#18c29c;'
+    Open.style.left = '45px'
+    logo.style.removeProperty('visibility')
+    gridtxt.style.removeProperty('visibility')
+    pettxt.style.removeProperty('visibility')
+    usertxt.style.removeProperty('visibility')
+    vettxt.style.removeProperty('visibility')
+    settingstxt.style.removeProperty('visibility') 
+}
+function back_off(){
+    Open.style.left = '0px'
+    Side.style.width = '78px'
+    Side.style.backgroundColor ='#18c29c;'
+    logo.style.setProperty('visibility','hidden')
+    gridtxt.style.setProperty('visibility', 'hidden')
+    pettxt.style.setProperty('visibility', 'hidden')
+    usertxt.style.setProperty('visibility', 'hidden')
+    vettxt.style.setProperty('visibility', 'hidden')
+    settingstxt.style.setProperty('visibility', 'hidden')
+}   
+ 
